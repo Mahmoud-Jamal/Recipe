b1d2beb1cfc1bb1b6cebc41d90029885324de36e
Mahmoud-Jamal <mahmoud.gemy9966@gmail.com>
2025-02-09 11:46:45 +0200

    commit4

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,19 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
 import { DataService } from './../services/api/data.service';
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
-import { NgClass, NgFor } from '@angular/common';
 interface man {
   strCategory: string;
 }
 @Component({
   selector: 'app-button',
-  imports: [RouterLink, RouterLinkActive, NgFor, NgClass],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
+  selectedValue: string = '';
   data: man[] = [];
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private router: Router) {}
+  onSelectChange() {
+    if (this.selectedValue) {
+      // Navigate to the new URL with the selected value as a parameter
+      this.router.navigate(['/category', this.selectedValue]);
+    }
+  }
   ngOnInit(): void {
     this.dataService.getCategory().subscribe({
       next: (res) => {
